7f1711884c29d4fcfe8c0b43cf6d6f73b621f0ca
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-05 22:07:28 +0300

    Add files via upload

---
+++ infrastructure/utils/tiny_url_shortener.py
@@ -0,0 +1,22 @@
+import logging
+import urllib.parse
+import requests
+
+
+class TinyURLShortener:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.URL = "https://tinyurl.com/api-create.php"
+
+    def shorten(self, url_long):
+        try:
+            url = self.URL + "?" + urllib.parse.urlencode({"url": url_long})
+            res = requests.get(url)
+            if res.status_code == 200:
+                return res.text
+            else:
+                raise Exception(f"Failed to shorten URL. Status code: {res.status_code}")
+        except requests.exceptions.RequestException as e:
+            raise requests.exceptions.RequestException(f"Request failed: {e}")
+        except Exception as e:
+            raise Exception(f"An error occurred: {e}")
