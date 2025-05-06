6e31dd7357d4f97648b302e127639913e5db0860
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-06 11:02:06 +0300

    Update pretty_print.py

--- infrastructure/utils/pretty_print.py
+++ infrastructure/utils/pretty_print.py
@@ -4,8 +4,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def print_in_format(data: any, log_level: int = logging.INFO) -> None:
-    formatted_data = json.dumps(data, indent=4, sort_keys=True, default=str)
+def print_in_format(
+    data: any,
+    log_level: int = logging.INFO,
+    indent: int = 4,
+    sort_keys: bool = True,
+    default: callable = None
+) -> None:
+    formatted_data = json.dumps(data, indent=indent, sort_keys=sort_keys, default=default)
 
     level_name = logging.getLevelName(log_level).lower()
     log_method = getattr(logger, level_name, logger.debug)
